cee143f5e51c498aa719d40263aacef4fad23840
ColleenCallonego <colleencallonego@gmail.com>
2021-01-13 18:06:07 +0100

    Correction classe Tree

--- V1pasFinie/Tree.java
+++ V1pasFinie/Tree.java
@@ -5,16 +5,22 @@ public class Tree {
     public String sticks;
     public Integer score;
     public Integer level;
-    public ArrayList<Arbre> sons;
+    public ArrayList<Tree> sons;
 
    public Tree (String sticks, Integer level){
        this.sticks = sticks;
        this.level = level;
+       sons = new ArrayList<Tree>();
    }
 
    public Tree (String sticks, Integer score, Integer level){
        this.sticks = sticks;
        this.score = score;
        this.level = level;
+       sons = new ArrayList<Tree>();
+   }
+
+   static Arbre CreateTree(){
+
    }
 }
